565443d8805eaaec704321c6836dfdc5b25fcd75
nathalyloor13 <jnloor@espe.edu.ec>
2026-08-24 17:27:59 -0500

    fix: fallback para inicializacion de supabase en build

--- lib/superbase.ts
+++ lib/superbase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://yadgsltfgsjmkmfcmfrz.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_4NLHnicktdkS9fILxrrMxg_pdvXuP3Z';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
